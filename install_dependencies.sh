67a86d81ff7538987496da817d9257f9e6d89350
0may <mayer@adbk-nuernberg.de>
2019-07-04 10:26:51 +0100

    updated dependencies and video position

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -7,9 +7,14 @@ apt-get upgrade -y
 echo "\n==== Installing python3 ===="
 apt-get install python3 python3-dev -y
 
-echo "\n==== Installing pip3 ===="
+echo "\n==== Installing/upgrading pip3 ===="
 apt-get install python3-pip -y
-pip3 install --upgrade pip
+python3 -m pip install --upgrade pip
+
+echo "\n==== Installing/upgrading setuptools and wheel ===="
+pip3 install setuptools
+pip3 install wheel
+python3 -m pip install --upgrade setuptools wheel
 
 echo "\n==== Installing GPIO module ===="
 pip3 install RPi.GPIO
@@ -22,8 +27,18 @@ echo "\n==== Installing pygame module ===="
 apt-get install libopenal1 libflac8 libmikmod3 libxi6 libdirectfb-1.2-9 libsdl-image1.2 libpng12-0 libsm6 libsdl-mixer1.2 libjbig0 libsdl-ttf2.0-0 libvorbisfile3 libogg0 libasyncns0 libmad0 libice6 libsdl1.2debian libxtst6 libvorbis0a libjack-jackd2-0 libpulse0 libfluidsynth1 libcaca0 libx11-xcb1 libsndfile1 libvorbisenc2 libopus0 libportmidi0 libtiff5 -y
 pip3 install pygame
 
+echo "\n==== Installing numpy module ===="
+pip3 install numpy
+
+echo "\n==== Installing picamera module with arrays ===="
+pip3 install "picamera[array]"
+
+echo "\n==== Installing opencv module ===="
+pip3 install opencv-contrib-python
+
 echo "\n==== Installing omxplayer ===="
 apt-get install omxplayer -y
 
 echo "\n==== Installing jupyter ===="
 pip3 install jupyter
+
